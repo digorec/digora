76ba893ae9d4b273cc12bb8f4d4923af36af1d38
digorec <rmzn@ya.ru>
2013-12-13 12:49:15 +0400

    begin

---
+++ middleware/templates.js
@@ -0,0 +1,60 @@
+var ejs = require('ejs')
+  , fs = require('fs')
+  , path = require('path');
+
+module.exports = function (views) {
+  return function (req, res, next) {
+    req.layout = 'layouts/global.ejs';
+    req.locals = {url: req.url};
+    res.render = function (view, data) {
+      
+      read(view, function (err, contents) {
+        if(err) return next(err);
+        
+        var body = render(contents, data);
+        read(req.layout, function (err, contents) {
+          if(err) return next(err);
+          var d = {body: body, locals: req.locals, data: data};
+          Object.keys(req.locals).forEach(function (k) {
+            d[k] = req.locals[k];
+          });
+          res.send(render(contents, d));
+        });
+      });
+    }
+    
+    next();
+  }
+  
+  function read(view, fn) {
+    fs.readFile(path.join(views, view), function (err, data) {
+      if(err) return fn(err);
+      fn(null, data.toString());
+    });
+  }
+
+  function partial(view, data) {
+    var tmpl = fs.readFileSync(path.join(views, 'partials', view + '.ejs'));
+    if(tmpl) tmpl = tmpl.toString();
+    return render(tmpl, data);
+  }
+
+  function render(template, data) {
+    data = data || {};
+    var content;
+    
+    try {
+      content = ejs.render(template, {body: data.body || '', data: data || {}, partial: partial, script: script});
+    } catch(e) {
+      console.error(e);
+    }
+    
+    return content;
+  }
+  
+  function script(f) {
+    var src = '<script src="' + '/javascripts' + '/' + f + '.js' + '"></script>';
+    return render(src);
+  }
+}
+
---
+++ routes/index.js
@@ -0,0 +1,130 @@
+var md = require('node-markdown').Markdown
+  , path = require('path')
+  , Query = require('../lib/query')
+  , redirects = require('../redirects.json');
+  
+// redirects
+app.get('/redirect', function (req, res) {
+  var redirect = redirects[req.param('url')];
+  console.log('redirect to', redirect);
+  
+  res.redirect(redirect || '/');
+});
+
+app.get('/search', function (req, res) {
+  var query = new Query(req.param('q'), app.index.root(), app.docs);
+  
+  var results = query.search();
+  
+  res.render('results.ejs', results);
+});
+
+app.get('/examples', function (req, res) {
+  var examples = [];
+  
+  app.index.root().children().forEach(function (c) {
+    if(c.dir) {
+      c.children().forEach(function (cc) {
+        if(cc.basename() === 'examples') examples.push(cc);
+      });
+    }
+  });
+  
+  res.render('examples.ejs', {examples: examples});
+});
+
+app.get('/docs', function (req, res) {
+  res.redirect('/');
+});
+
+app.get('/modules', function (req, res, next) {
+  var info = app.docs['docs/using-modules/official'];
+  
+  res.render('modules.ejs', {info: info});
+});
+
+app.get('/:page', function (req, res, next) {
+  var page = req.param('page');
+  var info = app.docs['docs/' + page];
+  var root = app.index.root();
+  var data = {info: info, page: page, root: root};
+  
+  if(page === 'api') {
+    data.refs = root.subReferences();
+  }
+  
+  if(~['api', 'guides', 'modules'].indexOf(page)) {
+    res.render('page.ejs', data);
+  } else {
+    next();
+  }
+
+});
+
+app.get('/', function (req, res) {
+  var info = app.docs['docs/index.md'];
+  
+  res.render('page.ejs', {info: info});
+});
+
+app.get(/^\/docs\/(.+)$/, function (req, res, next) {
+  var p = req.params[0]
+    , info = app.docs['docs/' + p]
+    , refs = []
+    , mainParent = info && info.mainParent();
+
+  if(!info) {
+    return next();
+  }
+
+  if (info.dir && req.param('include') !== 'all') {
+    var children = info.children(true);
+    if(children) {
+      for (var i=0; i < children.length; i++) {
+        if(children[i].file) {
+          res.redirect(children[i].url());
+          return;
+        }
+      };
+    }
+  }
+  
+  app.index.root().children(true).forEach(function (c) {
+    if(c == mainParent) {
+      c.children().forEach(function (cc) {
+        var bn = cc.basename();
+        if(cc.dir && bn === 'reference' || bn === 'internal-api') {
+          refs.push(cc);
+        }
+      });
+    }
+  });  
+      
+  res.render('browser.ejs', {refs: refs, info: info, include: req.param('include'), current: req.url});
+});
+
+
+app.get('/complete/:input', function (req, res) {
+  var input = req.param('input');
+  var terms = [];
+  
+  var query = new Query(input, app.index.root(), app.docs);
+  
+  res.send(query.phrases());
+});
+
+function formatPreview(query, str) {
+  if(str[0] === '#') return;
+  
+  var words = query.split(/\s/);
+  str = md(str);
+  
+  words.forEach(function (w) {
+    var r = '(' + w + ')';
+    str = str.replace(RegExp(r, 'gi'), '<strong class="kw-match">$1</strong>');
+  });
+  
+  return str;
+}
+
+
